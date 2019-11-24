8e9e7d1f000061182f3bc42f69d485ff56933a79
Florentín Pérez González <alu0101100654@ull.edu.es>
2019-11-24 12:59:08 +0000

    Se espera que Plate tenga una lista de ingredientes

--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -426,8 +426,20 @@ end
 
 RSpec.describe Food::Plate do
   describe "Pruebas iniciales sobre Plate" do
-    it "Plate tiene un cosntructor con 3 argumentos" do
+    it "Plate tiene un constructor con 3 argumentos" do
       expect(Food::Plate).to respond_to(:new).with(3).arguments
     end
+    before :each do
+      @huevos = Food::Food.new({:name => 'Huevos', :protein => 13.0, :carbohydrates => 1.1, :lipids => 11.0, :gas => 4.2, :land_use => 5.7})
+      @cordero = Food::Food.new({:name => 'Cordero', :protein => 18.0, :carbohydrates => 0.0, :lipids => 17.0, :gas => 20.0, :land_use => 185.0})
+      @lentejas = Food::Food.new({:name => 'Lentejas', :protein => 23.5, :carbohydrates => 52.0, :lipids => 1.4, :gas => 0.4, :land_use => 3.4})
+      @lista = Food::List.new
+      @lista.pushVarious([@huevos, @cordero, @lentejas])
+      @lista_grams = Food::List.new
+      @lista_grams.pushVarious([10, 20, 30])
+    end
+      it "Plate tiene definido un conjunto de alimentos" do
+        expect(Food::Plate.new("Lentejas Deluxe", @lista, @lista_grams)).to respond_to(:ingredients)
+      end
   end
 end
