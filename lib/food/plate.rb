2e42da89b02f7277eb868947a3b0c8e42994e056
Florentín Pérez González <alu0101100654@ull.edu.es>
2019-11-24 13:01:14 +0000

    Plate tiene una lista de gramos para cada ingrediente

--- lib/food/plate.rb
+++ lib/food/plate.rb
@@ -1,6 +1,6 @@
 module Food
     class Plate
-        attr_reader :ingredients
+        attr_reader :ingredients, :ingredients_grams
         def initialize(name, ingredients, ingredients_grams)
 
         end
