22eec94c8ea80c448b768c1b4d59e20aba42d88f
Florentín Pérez González <alu0101100654@ull.edu.es>
2019-11-25 17:19:04 +0000

    Se obtiene correctamente los gases de efecto invernadero en kg generados para la creación del plato

--- lib/food/impact_plate.rb
+++ lib/food/impact_plate.rb
@@ -1,11 +1,19 @@
 module Food
     class Impact_Plate < Plate
-        def calculate_impact
-
+        attr_reader :co2_impact, :land_use
+        def initialize(name, ingredients, ingredients_grams)
+            super(name, ingredients, ingredients_grams)
+            @co2_impact =  calculate_impact
         end
 
-        def land_use
+        private
 
+        def calculate_impact
+            co2_kg = 0.0
+            @ingredients.each_with_index do |x, index|
+                co2_kg = co2_kg + (x.gases*100.round * @ingredients_grams[index]*100.round).truncate(3) / 10000000.0
+            end
+            co2_kg
         end
     end
 end
