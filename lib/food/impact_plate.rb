2dd3ebc46381a41be747b0458c2c01e188b066d1
Florentín Pérez González <alu0101100654@ull.edu.es>
2019-11-25 18:40:58 +0000

    Modificación <=> en Impact_Plate para considerar el impacto ambiental

--- lib/food/impact_plate.rb
+++ lib/food/impact_plate.rb
@@ -13,12 +13,17 @@ module Food
             string << "CO2: #{@co2_impact}, m2: #{@land_use})"
         end
 
+        def <=>(other)
+            return nil unless other.instance_of? Impact_Plate
+            [@co2_impact, @land_use, self.calculate_TCV] <=> [other.co2_impact, other.land_use, other.calculate_TCV]
+        end
+
         private
 
         def calculate_impact
             co2_kg = 0.0
             @ingredients.each_with_index do |x, index|
-                co2_kg = co2_kg + (x.gases*100.round * @ingredients_grams[index]*100.round).truncate(3) / 10000000.0
+                (co2_kg = co2_kg + (x.gases*100.round * @ingredients_grams[index]*100.round).truncate(3) / 10000000.0).round(3)
             end
             co2_kg
         end
