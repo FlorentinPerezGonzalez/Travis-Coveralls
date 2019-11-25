bba4512ae957cb44b82c64cae585127f432e5744
Florentín Pérez González <alu0101100654@ull.edu.es>
2019-11-25 17:23:59 +0000

    El método land_use devuelve la estimación de terreno correcta

--- lib/food/impact_plate.rb
+++ lib/food/impact_plate.rb
@@ -4,6 +4,7 @@ module Food
         def initialize(name, ingredients, ingredients_grams)
             super(name, ingredients, ingredients_grams)
             @co2_impact =  calculate_impact
+            @land_use = calculate_land_use
         end
 
         private
@@ -14,6 +15,14 @@ module Food
                 co2_kg = co2_kg + (x.gases*100.round * @ingredients_grams[index]*100.round).truncate(3) / 10000000.0
             end
             co2_kg
+        end
+
+        def calculate_land_use
+            land_use = 0.0
+            @ingredients.each_with_index do |x, index|
+                land_use = land_use + (x.land_use*100.round * @ingredients_grams[index]*100.round).truncate(3) / 10000000.0
+            end
+            land_use
         end
     end
 end
